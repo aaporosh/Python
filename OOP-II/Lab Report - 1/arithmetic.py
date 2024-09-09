d59a3537f5ad6adc8ffcafd4bcb0b6e299074d38
Md Abir Ahmed Porosh <aaporosh02@gmail.com>
2024-09-09 08:47:23 +0600

    Update arithmetic.py

---
+++ OOP-II/Lab Report - 1/arithmetic.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the 1st number : "))
+num2 = int(input("Enter the 2nd number : "))
+
+sum = num1 + num2
+sub = num1 - num2
+mul = num1*num2
+div = float(num1/num2)
+print(f'Addition : {sum}')
+print(f'Substraction : {sub}')
+print(f'multiplication : {mul}')
+print(f'Divition : {div}')
