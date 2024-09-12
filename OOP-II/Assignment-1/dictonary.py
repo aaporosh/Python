ee110508f51490985da9949e20447eb00209e7b7
Md Abir Ahmed Porosh <aaporosh02@gmail.com>
2024-09-12 21:22:32 +0600

    Dictionary

---
+++ OOP-II/Assignment-1/dictonary.py
@@ -0,0 +1,11 @@
+course = {
+    "CSE101" : { "Course name" : "Introduction to Programming",
+            "Credits": 3,
+            "Instructor": "Dr. Alice"},
+    "CSE102"  : {"Course name" : "Data Structures",
+                 " Credits" : 4, 
+                 "Instructor" : "Dr. Bob"},
+    "CSE103" : { "Course name" : "Database Systems",
+                " Credits" : 3, 
+                "Instructor" : "Dr.Carol"}
+}
