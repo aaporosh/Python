ff86f4baedb33e74d4fb86dd56e0f2a4ec555a17
Md Abir Ahmed Porosh <aaporosh02@gmail.com>
2024-09-12 21:30:51 +0600

    leap year

--- OOP-II/Lab Report - 1/leap.py
+++ OOP-II/Lab Report - 1/leap.py
@@ -1,2 +1,8 @@
 year = int(input("Year : "))
 
+if year%400==0 and year%200==0 :
+    print(f'{year} Year is Leap Year')
+elif year%4==0 and year%100!=0:
+    print(f'{year} Year is Leap Year')
+else  :
+    print(f'{year} Year is not Leap Year')
