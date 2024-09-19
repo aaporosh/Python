c3ab7bbf432a4d90576288c9fb0eb039e019d66c
Md Abir Ahmed Porosh <aaporosh02@gmail.com>
2024-09-19 19:48:11 +0600

    uppercase to lowercase lowercase to uppercase

---
+++ OOP-II/Lab Report - 1/str.py
@@ -0,0 +1,8 @@
+string = str(input("Lowercase String : "))
+
+#lowercase to uppercase 
+print(f'Upper case : {string.upper()}\n')
+
+#uppercase to lower case
+s = str(input("Uppercase String : "))
+print(f'Lower case : {s.lower()}\n')
