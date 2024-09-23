e9e8ea6cc13991587a07e5d138899d680a96148a
Md Abir Ahmed Porosh <aaporosh02@gmail.com>
2024-09-23 20:09:03 +0600

    transpose of a matrix

---
+++ OOP-II/Lab Report - 1/trans.py
@@ -0,0 +1,11 @@
+x = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+
+result = [[x[j][i] 
+           
+        for j in range(len(x))] 
+            for i in range(len(x[0]))]
+
+for r in result:
+   print(r)
